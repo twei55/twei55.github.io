a055567e3aa62d08a44505a5990d2791bc4fe8d3
Tobias Weiß <tobias.weiss@gmail.com>
2017-04-07 17:49:46 +0200

    Extend service worker to ask for notification permission

--- amp-playground/sw.js
+++ amp-playground/sw.js
@@ -1,1 +1,31 @@
-console.log('Hello from Service worker');
+'use strict';
+
+if ('showNotification' in ServiceWorkerRegistration.prototype) {
+  navigator.serviceworker.ready
+  .then(registration => {
+    return registration.pushManager.subscribe({
+      userVisibleOnly: true
+    });
+  })
+  .then(subscription => {
+    // Do something with the subscription.
+  })
+  .catch(error => {
+    // Do something with the error.
+  });
+}
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  var title = 'Hello from Babbel Magazine.';
+  var body = 'We have a new article';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+    })
+  );
+});
+
+
